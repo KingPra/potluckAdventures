cf225399ef2ab8b8eeace4720dda8ab471dc1cd5
King Pra <kingi.pra@gmail.com>
2019-05-30 17:26:19 -0500

    updated files

--- src/main/java/com/tts/potluckAdventures/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/potluckAdventures/BlogPost/BlogPostController.java
@@ -1,14 +1,32 @@
 package com.tts.potluckAdventures.BlogPost;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class BlogPostController {
-	@GetMapping("/")
+
+	@Autowired
+	private BlogPostRepository blogPostRepository;
+
+	@GetMapping(value = "/")
 	public String index(BlogPost blogPost) {
 		return "blogpost/index";
+	}
+
+	private BlogPost blogPost;
+
+	@PostMapping(value = "/")
+	public String addNewBlogPost(BlogPost blogPost, Model model) {
+		blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		model.addAttribute("title", blogPost.getTitle());
+		model.addAttribute("author", blogPost.getAuthor());
+		model.addAttribute("blogEntry", blogPost.getBlogEntry());
+		return "blogpost/result";
 	}
 }
---
+++ src/main/java/com/tts/potluckAdventures/BlogPost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.tts.potluckAdventures.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+}
