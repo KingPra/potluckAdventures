7f3bb6b609ee318773017a146bcf08af1e0ae356
King Pra <kingi.pra@gmail.com>
2019-06-07 08:25:38 -0500

    refactored controller and added sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into blog_post 
+    values(1, 'title one', 'king', 'hey people');
+    insert into blog_post
+    values(2, 'title two', 'king', 'hey people hiiii');
+    insert into blog_post
+    values(3, 'title three', 'king', 'hey people 1230');
+    insert into blog_post
+    values(4, 'title four', 'king', 'hey people');
